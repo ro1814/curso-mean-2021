7d7f365c42dd56907922ae5d96c5484713005938
ro1814 <rnsm14@gmail.com>
2021-10-21 12:20:56 +0200

    Angular properties 2.0

---
+++ 04-Angular/Ej01-Angular/src/app/entidades/disco.ts
@@ -0,0 +1,10 @@
+
+export class Disco {
+
+    public constructor(
+            public titulo   : string|null = null, 
+            public grupo : string|null = null,
+            public year     : number|null = null){
+        }
+
+}
