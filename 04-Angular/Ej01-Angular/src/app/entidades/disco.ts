024394284023ceb8402684fd52d99b789b7bc51f
ro1814 <rnsm14@gmail.com>
2021-10-23 12:22:12 +0200

    Updating files, creating new project

--- 04-Angular/Ej01-Angular/src/app/entidades/disco.ts
+++ 04-Angular/Ej01-Angular/src/app/entidades/disco.ts
@@ -2,9 +2,15 @@
 export class Disco {
 
     public constructor(
-            public titulo   : string|null = null, 
-            public grupo : string|null = null,
-            public year     : number|null = null){
+            public titulo : string|null = null, 
+            public grupo  : string|null = null,
+            public year   : number|null = null){
         }
+
+    public vaciar(){
+        this.titulo = null
+        this.grupo  = null
+        this.year   = null
+    }
 
 }
