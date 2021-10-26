126e5c8b9377436c54ed6cda48790fb13c8380ea
ro1814 <rnsm14@gmail.com>
2021-10-26 10:15:45 +0200

    4rd project done

---
+++ 04-Angular/Ej04-Servicios/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { FormularioDiscosComponent } from './componentes/formularioDiscos/formulariodiscos.component';
+import { ListadoDiscosComponent } from './componentes/listadoDiscos/listadodiscos.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { PieComponent } from './componentes/pie/pie.component';
+
+let rutas:Routes = [
+  {
+    path: "discos/listado",
+    component: ListadoDiscosComponent
+  },
+  {
+    path: "discos/formulario",
+    component: FormularioDiscosComponent
+  }
+  
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PieComponent,
+    MenuComponent,
+    CabeceraComponent,
+    ListadoDiscosComponent,
+    FormularioDiscosComponent
+  ],
+  imports: [
+    BrowserModule, //Esta siempre en una app Web
+    FormsModule, //Para el Bidirectional binding
+    RouterModule.forRoot(rutas) //Para los router-outlets
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
