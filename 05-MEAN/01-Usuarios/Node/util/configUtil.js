ea40b04fca775618b32c2b3be9b64c754d3cc48c
ro1814 <rnsm14@gmail.com>
2021-10-28 14:07:48 +0200

    Setting e-commerce project

---
+++ 05-MEAN/01-Usuarios/Node/util/configUtil.js
@@ -0,0 +1,13 @@
+const fs = require("fs")
+
+console.log("Leyendo la configuracion...")
+
+let buffer = fs.readFileSync("conf.json")
+let json = buffer.toString()
+let conf = JSON.parse(json)
+
+exports.conf = conf
+
+
+
+
