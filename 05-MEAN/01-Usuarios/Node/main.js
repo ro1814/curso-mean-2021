13bf56d3382b58d148b5b8e8c8b84f4a12685f13
ro1814 <rnsm14@gmail.com>
2021-10-29 13:57:17 +0200

    Promises, mongo rest

--- 05-MEAN/01-Usuarios/Node/main.js
+++ 05-MEAN/01-Usuarios/Node/main.js
@@ -1,15 +1,43 @@
-const conf = require("./util/configUtil").conf
-const mongoDBUtil = require("./bbdd/mongoDBUtil")
-const http = require('http')
-const express = require("express")
-
 console.log("Inicializando...")
 
-mongoDBUtil.conectarBBDD()
+//Librerías/////////////////////////////////////////////////
+const http = require('http')
+const express = require("express")
+//Librerías 'nuestras'//////////////////////////////////////
+const mongoDBUtil = require("./bbdd/mongoDBUtil")
+//Routers para express//////////////////////////////////////
+const usuariosRouter = require("./rest/usuariosREST").router
 
+//Primer paso: leer el fichero de configuración/////////////
+require("./util/configUtil")
 
-//console.log(conf.app_puerto)
+//Segundo paso: conectar con la base de datos///////////////
+mongoDBUtil
+    .conectarBBDD()
+    .then(function(){
+        //Tercer paso: arrancar el servidor/////////////////
+        arrancarServidor()
+    })
+    .catch(function(){
+        console.log("Parando la aplicacion...")
+        process.exit(2)
+    })
 
+function arrancarServidor(){
+    console.log("Arrancando el servidor...")
+
+    let app = express()
+
+    //middleware
+
+    //Routers
+    app.use(usuariosRouter)
+
+    http.createServer(app).listen(process.env.app_puerto, function(){
+        console.log("Esperando peticiones en el puerto "+process.env.app_puerto)
+    })
+
+}
 
 
 
