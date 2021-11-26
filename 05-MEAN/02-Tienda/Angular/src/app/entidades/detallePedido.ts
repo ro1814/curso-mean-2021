49359944c7a2bee283fc11c7d82f0452b7c5f7ee
ro1814 <rnsm14@gmail.com>
2021-11-26 15:26:37 +0100

    store working, must add products in order to see catalogue

---
+++ 05-MEAN/02-Tienda/Angular/src/app/entidades/detallePedido.ts
@@ -0,0 +1,11 @@
+import { Producto } from "./producto";
+
+export class DetallePedido {
+
+    public constructor(
+        public producto:Producto|null = null,
+        public cantidad:number = 0,
+        public precio:number|null = null,
+    ){}
+
+}
---
+++ 05-MEAN/02-Tienda/Angular/src/app/entidades/pedido.ts
@@ -0,0 +1,60 @@
+import { DetallePedido } from "./detallePedido";
+import { Producto } from "./producto";
+import { Usuario } from "./usuario";
+
+export class Pedido {
+
+    public constructor(
+        public _id      : string|null  = null,
+        public codigo   : string|null  = null,
+        public fecha    : string|null  = null,
+        public estado   : string|null  = null,
+        public direccion: string|null  = null,
+        public usuario  : Usuario|null = null,
+        public detalles : DetallePedido[] = [],
+        public total    : number|null  = null
+    ){}
+
+    public addProducto(producto:Producto):void{
+        let detalle:any = null
+        for(let detalleAux of this.detalles){
+          if(detalleAux?.producto?._id == producto._id){
+            detalle = detalleAux
+            break
+          }
+        }
+        if(detalle){
+          detalle.cantidad++
+        } else {
+          detalle = new DetallePedido(producto, 1, producto.precio)
+          this.detalles?.push(detalle)
+        }
+    
+        this.calcularTotal()      
+    } 
+    
+    public quitarProducto(producto:Producto):void{
+        
+        for(let a=0; a<this.detalles.length; a++){
+          let detalle:DetallePedido = this.detalles[a]
+          if(detalle.producto?._id == producto._id){
+            detalle.cantidad--
+            if(detalle.cantidad == 0){
+              this.detalles.splice(a,1)
+            }
+          }
+        }
+
+        this.calcularTotal
+    } 
+        
+    private calcularTotal():void{
+        let detalle:any
+        let total:number = 0
+        for(detalle of this.detalles){
+          total += detalle.cantidad*detalle.precio
+        }
+        this.total = total
+    }
+
+}
